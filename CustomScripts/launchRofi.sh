0a0ddbdb193257f2961ca6ff990278d3bd845f5e
Vitor Andrade Guidorizzi <vitor.guidorizzi@hotmail.com>
2019-02-04 18:00:22 -0300

    moarfixes

--- CustomScripts/launchRofi.sh
+++ CustomScripts/launchRofi.sh
@@ -3,4 +3,4 @@ bg_color=#F5000000
 text_color=#f3f4f5
 htext_color=#00AA00
 
-rofi -show drun -lines 5 -eh 2 -width 30 -show-icons -padding 20 -bw 0 -color-window "$bg_color, $bg_color, #00ffffff" -color-normal "#00000000, $text_color, #00000000, #00000000, $htext_color" -font "Ubuntu Display 13" 
+rofi -modi run,drun,window -show drun -show-icons -sidebar-mode -lines 5 -eh 2 -scroll-method 1 -width 30 -show-icons -padding 20 -bw 0 -color-window "$bg_color, $bg_color, #00ffffff" -color-normal "#00000000, $text_color, #00000000, #00000000, $htext_color" -font "Ubuntu Display 13" 
